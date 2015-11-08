8fc0dab329777b17d12719d85278dd8d68fd47a9
Jing Liu <cpujing@gmail.com>
2015-11-08 23:17:29 +0100

    Add MasterConf serialization test
    - Add MasterConf serialization test

--- test/EMCFW_Test/Serialization/TestSerialization.cc
+++ test/EMCFW_Test/Serialization/TestSerialization.cc
@@ -1,6 +1,7 @@
 #include "TestSerialization.hh"
 #include <gtest.h>
 #include "../../../dataset/control/LoadSpec.hh"
+#include "../../../node/master/MasterConf.hh"
 
 TestSerialization::TestSerialization()
 {
@@ -18,6 +19,7 @@ void TestSerialization::TearDown()
 
 TEST(TestSerialization, testLoadSpec)
 {
+    printf("Testing LoadSpec\n");
     DataSet::Control::LoadSpec before;
 
     before.setConfigFilePath("/tmp/fakedPath.conf");
@@ -44,5 +46,29 @@ TEST(TestSerialization, testLoadSpec)
     EXPECT_EQ (before.getNodeCluster()[0], after.getNodeCluster()[0]);
     EXPECT_EQ (before.getNodeCluster()[1], after.getNodeCluster()[1]);
     EXPECT_EQ (before.getNodeCluster()[2], after.getNodeCluster()[2]);
+}
+
+TEST(TestSerialization, testMasterConf)
+{
+    Node::Master::MasterConf before;
+
+    before.dataStartIndex = 1;
+    before.dataEndIndex = 100;
+    before.dataFileDir = "/tmp/fakedDir";
+    before.dispatchStrategy = 12;
+
+    before.serialize();
+    const char * result = before.getSerializer().getPackedString();
+
+    Node::Master::MasterConf after;
+
+    after.getSerializerRef().setPackedString(result);
+
+    after.deserialize();
+
+    EXPECT_EQ (before.dataStartIndex, after.dataStartIndex);
+    EXPECT_EQ (before.dataEndIndex, after.dataEndIndex);
+    EXPECT_EQ (before.dataFileDir.compare(after.dataFileDir), 0);
+    EXPECT_EQ (before.dispatchStrategy, after.dispatchStrategy);
 }
 
